23f82f3cec6b8b44d660b853782d71bcb1e31e14
M0lk1L4b <chantalcarballo@gmail.com>
2022-05-23 16:45:17 -0300

    slider terminado

--- JS/slider.js
+++ JS/slider.js
@@ -1,7 +1,28 @@
 (function(){
-    const sliders = [...document.querySelectorAll('tipos_jabones__body')]
-    const buttonNext = document.qu
-    console.log(sliders)
 
-})();
- 
+    const sliders = [...document.querySelectorAll('.tipos_jabones__body')]
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    });
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    });
+
+    const changePosition = (add)=>{
+        const currentTipos_jabones = document.querySelector('.tipos_jabones__body--show').dataset.id; 
+        value =  Number( currentTipos_jabones);
+        value+= add;
+        
+        sliders[Number(currentTipos_jabones)-1].classList.remove('tipos_jabones__body--show')
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length : 1;
+         }
+
+         sliders[value-1].classList.add('tipos_jabones__body--show')
+        }
+
+    })();
